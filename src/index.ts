0925c53e4577916a0b9b324a33655623c64d232a
Tommy Giardina <tommy.giardina@gmail.com>
2020-12-03 12:46:40 -0600

    Removed seed
    Seed's functionality is handled by inversify's onActivation.

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,3 @@ export { default as addDetour } from "./detour";
 export { default as addLogger } from "./logger";
 export { default as addQueue } from "./queue";
 export { default as addTranslator } from "./translator";
-export { default as seed } from "./seed";
